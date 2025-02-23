551e9f807991076f65ce9f824d6ccd56f3d75100
Joel-x-x <acostajoel0001@gmail.com>
2025-02-23 17:15:50 -0500

    feat: first commit

---
+++ src/main/java/com/ferrientregas/FerrientregasApplication.java
@@ -0,0 +1,13 @@
+package com.ferrientregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerrientregasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FerrientregasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ferrientregas/FerrientregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferrientregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerrientregasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
