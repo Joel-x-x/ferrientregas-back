5a42c7e86117ab21b5e57dc0c27dab2c89f6d006
HuesosAce <pauloarielyepez@gmail.com>
2025-03-15 22:48:58 -0500

    fix: add findByName method in repository

--- src/main/java/com/ferrientregas/deliverystatus/DeliveryStatusRepository.java
+++ src/main/java/com/ferrientregas/deliverystatus/DeliveryStatusRepository.java
@@ -3,8 +3,10 @@ package com.ferrientregas.deliverystatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DeliveryStatusRepository extends JpaRepository<DeliveryStatusEntity, UUID> {
     List<DeliveryStatusEntity> findAllByDeletedFalse();
+    DeliveryStatusEntity findByName(String name);
 }
